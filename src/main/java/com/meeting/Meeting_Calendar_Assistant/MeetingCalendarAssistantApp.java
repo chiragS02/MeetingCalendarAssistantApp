29e9640e8e347f55df8cf1167fda061ca763c5de
chirag <chiragsurendra2002@gmail.com>
2024-11-09 20:50:36 +0530

    Pushed code

---
+++ src/main/java/com/meeting/Meeting_Calendar_Assistant/MeetingCalendarAssistantApp.java
@@ -0,0 +1,13 @@
+package com.meeting.Meeting_Calendar_Assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingCalendarAssistantApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingCalendarAssistantApp.class, args);
+	}
+
+}
